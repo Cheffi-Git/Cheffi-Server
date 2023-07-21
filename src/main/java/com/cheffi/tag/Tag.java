1060c5ec8500190184f0ad316a489c12c04c49f9
lsh <seulee0862@gmail.com>
2023-07-21 09:27:11 +0900

    Feat: add Tag Entity

---
+++ src/main/java/com/cheffi/tag/Tag.java
@@ -0,0 +1,17 @@
+package com.cheffi.tag;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String type;
+    private String name;
+}
